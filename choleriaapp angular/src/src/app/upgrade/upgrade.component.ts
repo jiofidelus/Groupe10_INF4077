b08243a670fb8aebd9923e745297c1b1ba6536ea
talombouopdaarnaud <talombouopdaarnaud@gmail.com>
2021-02-09 10:15:26 +0100

    push 2

---
+++ choleriaapp angular/src/src/app/upgrade/upgrade.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upgrade',
+  templateUrl: './upgrade.component.html',
+  styleUrls: ['./upgrade.component.css']
+})
+export class UpgradeComponent implements OnInit {
+  
+  constructor() { }
+
+  ngOnInit() {
+    //alert("ok mercie ser55");
+//     var video = document.querySelector("#videoElement");
+
+// if (navigator.mediaDevices.getUserMedia) {
+//   navigator.mediaDevices.getUserMedia({ video: true })
+//     .then(function (stream) {
+//       console.log(video);
+//       //video.srcObject = stream;
+//     })
+//     .catch(function (err0r) {
+//       console.log("Something went wrong!sss");
+//     });
+// }
+  }
+
+}
